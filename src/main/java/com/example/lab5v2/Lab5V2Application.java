f0370096c835ae7e9a1cc51e75a563fea7c7dee5
baspop01 <63070115@it.kmitl.ac.th>
2022-09-12 23:10:56 +0700

    Initial commit

---
+++ src/main/java/com/example/lab5v2/Lab5V2Application.java
@@ -0,0 +1,13 @@
+package com.example.lab5v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5V2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5V2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab5v2/Lab5V2ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.lab5v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5V2ApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5V2Application.class, args);
+    }
+
+}
